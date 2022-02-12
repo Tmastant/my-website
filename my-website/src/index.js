476dec2593d6e4af3508bf51cc0599c8c7e13875
tmastant <tmastant@uwaterloo.ca>
2022-02-12 02:47:14 -0500

    small

--- my-website/src/index.js
+++ my-website/src/index.js
@@ -7,7 +7,7 @@ class InfoBox extends React.Component {
       return (
           <div>
             <h2>Hi! I'm</h2>
-            <h1>Thomas Mastantuono</h1>
+            <b><h1>Thomas Mastantuono</h1></b>
             <h2>I make games and programs</h2>
           </div>
       );
